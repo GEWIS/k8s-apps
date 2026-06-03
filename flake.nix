af15eae7d6e8122286f1b79a0f1fd57c6b0e42c6
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-06-03 19:54:22 +0200

    feature: test webhost

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "k8s-apps dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            kubeseal
+            kustomize
+            kubectl
+          ];
+        };
+      }
+    );
+}
